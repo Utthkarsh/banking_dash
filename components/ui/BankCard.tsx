5689b671963e6b349019a19c2e4fb776bc9c8446
Utthkarsh Kottali <ukottali@umass.edu>
2024-07-06 21:59:56 -0400

    display all credit cards in my banks page

--- components/ui/BankCard.tsx
+++ components/ui/BankCard.tsx
@@ -3,10 +3,11 @@ import Link from 'next/link'
 import { format } from 'path'
 import Image from 'next/image'
 import { formatAmount } from '@/lib/utils'
+import Copy from './Copy'
 const BankCard = ({account,userName,showBalance=true}:CreditCardProps) => {
   return (
     <div className="flex flex-col">
-      <Link href="/" className="bank-card">
+      <Link href={`/transaction-history/?id=${account.appwriteItemId}`} className="bank-card">
         <div className="bank-card_content">
           <div>
             <h1 className="text-16 font-semibold text-white">
@@ -22,7 +23,7 @@ const BankCard = ({account,userName,showBalance=true}:CreditCardProps) => {
               <h2 className="text-12 font-semibold text-white"> ●● / ●● </h2>
             </div>
             <p className="text-14 font-semibold tracking-[1.1px] text-white">
-              ●●●● ●●●● ●●●● <span className="text-16">{account.mask} </span>
+              ●●●● ●●●● ●●●● <span className="text-16">{account?.mask} </span>
             </p>
           </article>
         </div>
@@ -44,6 +45,7 @@ const BankCard = ({account,userName,showBalance=true}:CreditCardProps) => {
           className="absolute top-0 left-0"
         />
       </Link>
+      {showBalance && <Copy title={account?.sharableId}/>}
     </div>
   );
 }
